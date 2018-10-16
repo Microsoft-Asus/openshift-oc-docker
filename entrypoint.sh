44fd37395bd2d0d4d3c7a036c5794c0ea717d774
Jamie Jones <jbjonesjr@github.com>
2018-10-16 06:19:16 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,8 +7,10 @@ output=$( sh -c "oc $*" )
 # Preserve output for consumption by downstream actions
 echo "$output" > "${HOME}/${GITHUB_ACTION}.${OC_DEFAULT_OUTPUT}"
 
-# if $0 == login ....
-cp -r /root/.kube "${HOME}/.kube"
-
 # Write output to STDOUT
 echo "$output"
+
+# if $0 == login ....
+#cp -r /root/.kube "${HOME}/.kube"
+
+ls -la /root/
