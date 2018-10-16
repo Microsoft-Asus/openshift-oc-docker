0a0b63fc945069190913b220e93b32e983652ac0
Jamie Jones <jbjonesjr@github.com>
2018-10-16 06:14:33 -0700

    fix .kube directory location

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,12 +4,11 @@ set -e
 
 # Capture output
 output=$( sh -c "oc $*" )
-
 # Preserve output for consumption by downstream actions
 echo "$output" > "${HOME}/${GITHUB_ACTION}.${OC_DEFAULT_OUTPUT}"
 
 # if $0 == login ....
-cp -r /root/kube "${HOME}/kube"
+cp -r /root/.kube "${HOME}/.kube"
 
 # Write output to STDOUT
 echo "$output"
