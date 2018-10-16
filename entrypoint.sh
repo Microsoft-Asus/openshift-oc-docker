fb167a2e9fa2e1feac038355ccfaec4857f8cc13
Jamie Jones <jbjonesjr@github.com>
2018-10-16 06:46:37 -0700

    remove debugging, print command

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,15 +3,10 @@
 set -e
 
 # Capture output
+echo "$> oc $*"
 output=$( sh -c "oc $*" )
 # Preserve output for consumption by downstream actions
 echo "$output" > "${HOME}/${GITHUB_ACTION}.${OC_DEFAULT_OUTPUT}"
 
 # Write output to STDOUT
 echo "$output"
-
-# if $0 == login ....
-#cp -r /root/.kube "${HOME}/.kube"
-
-ls -la $HOME
-ls -la "$HOME"
